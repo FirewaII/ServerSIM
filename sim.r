8dec155710ba22cb9024bd60ba3dbfcc3e3d76ac
FirewaII <othmane@ajdor.pro>
2018-05-24 01:47:02 +0200

    priority

--- sim.r
+++ sim.r
@@ -4,58 +4,82 @@ serverSim <- function(lambda, mu){
   debug   <- FALSE
   
   # time vars
-  t.end   <- 10^4   # duration of simulation
-  t.clock <- 0      # simulation time
+  endTime   <- 10^4   # duration of simulation
+  currentTime <- 0      # simulation time
   
   # simulation parameters
   # we will be using exponential distribution
-  queue             <- 0      # query queue
-  N                 <- 400    # query queue capacity
-  nextArrival       <- 0      # time for the next query arrival (reception)
-  nextDeparture     <- t.end  # time for the next query departure (service)
+  queue             <- 0        # query queue
+  fQ                <- 0        # fast queue
+  nQ                <- 0        # normal queue
+  sQ                <- 0        # slow queue
+
+  N                 <- 400      # total query queue(s) capacity
+  nextArrival       <- 0        # time for the next query arrival (reception)
+  nextDeparture     <- endTime  # time for the next query departure (service)
   
   totalArrivals     <- 0      # total number of received queries
+  totalFA           <- 0      # total number of fast received queries
+  totalNA           <- 0      # total number of normal received queries
+  totalSA           <- 0      # total number of slow received queries
+
   totalDepartures   <- 0      # total number of serviced queries
-  totalDropped      <- 0      # total number of dropped queries
-  
-  while (t.clock < t.end){
+  totalFD           <- 0      # total number of fast serviced queries
+  totalND           <- 0      # total number of normal serviced queries
+  totalSD           <- 0      # total number of slow serviced queries
+
+  busyTime          <- 0      # total time servicing queries
+
+  while (currentTime < endTime){
     # Query arrival
     if (queue < N && nextArrival < nextDeparture || queue == 0){
-      
         if (debug){
           print("[DEBUG] NEW QUERY ADDED [DEBUG]")
         }
-        queue = queue + 1
+        queueType = sample(0:3, 1)
+        if (queueType == 0){
+          fQ = fQ + 1
+          totalFA = totalFA + 1
+        } else if (queueType == 1){
+          nQ = nQ + 1
+          totalNA = totalNA + 1
+        } else {
+          sQ = sQ + 1
+          totalSA = totalSA + 1
+        }
+        queue = fQ + nQ + sQ
         totalArrivals = totalArrivals + 1
-        t.clock = nextArrival        
-        nextArrival = t.clock + rexp(1, lambda)
+        currentTime = nextArrival        
+        nextArrival = currentTime + rexp(1, lambda)
         if (queue == 1){
           # Only query in queue is directly serviced
           if (debug){
             print("[DEBUG] SERVICING ONLY QUERY IN QUEUE [DEBUG]")
           }
-          nextDeparture = t.clock + rexp(1, mu)
+          nextDeparture = currentTime + rexp(1, mu)
+          lastBusyTime = currentTime
         }
-      if (queue >= N){
-        # server queue has reached its maximum capacity
-        #cat("/!\\ MAXIMUM CAPACITY REACHED /!\\ \n")
-        if (debug){
-          print("[DEBUG] QUERY DROPPED [DEBUG]")
-        }
-        totalDropped = totalDropped + 1
-      }
     } else {
       # Queue departure
-      t.clock = nextDeparture
-      queue = queue - 1
+      # queue = queue - 1
+      if (fQ > 0){
+        fQ = fQ - 1
+      } else if (nQ > 0){
+        nQ = nQ - 1
+      } else {
+        sQ = sQ - 1
+      }
+      queue = fQ + nQ + sQ
+      currentTime = nextDeparture
       totalDepartures = totalDepartures + 1
       if (queue > 0){
         if (debug){
           print("[DEBUG] SERVICING QUERY [DEBUG]")
         }
-        nextDeparture = t.clock + rexp(1, mu)
+        nextDeparture = currentTime + rexp(1, mu)
       } else {
-        nextDeparture = t.end
+        nextDeparture = endTime
+        busyTime = busyTime - lastBusyTime + currentTime
       }
     }
     if(debug){
@@ -63,15 +87,15 @@ serverSim <- function(lambda, mu){
     }
   }
 
-  cat('Nombre de requêtes reçues: ', totalArrivals, "\n")
+  cat('Nombre de requêtes reçues: ', totalArrivals, "\n\t", totalFA," prioritaires\n\t", totalNA," normales\n\t", totalSA," lentes\n")
   cat('Nombre de requêtes traitées: ', totalDepartures, "\n")
-  cat('Nombre de requêtes perdues: ', totalDropped, "\n")
+  cat('Nombre de requêtes perdues: ', totalArrivals - totalDepartures, "\n")
   cat('Nombre de requêtes restantes à la fin de la simulation: ', queue, "\n")
-
+  cat('Temps total de service: ', busyTime/10^6, " secondes\n")
 }
 
 cat("- STARTING SIMULATION -\n")
-serverSim(lambda = 3, mu = 2)
+serverSim(lambda = 0.2, mu = 0.3)
 cat("- SIMULATION ENDED -")
 
 
